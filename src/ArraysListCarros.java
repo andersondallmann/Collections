3844f4ece6b89ae170bd3e2e5bffa624f115599a
andersondallmann <andersondallmann@hotmail.com>
2022-12-31 11:42:49 -0300

    Code updated!

--- src/ArraysListCarros.java
+++ src/ArraysListCarros.java
@@ -1,4 +1,4 @@
-import java.sql.SQLOutput;
+import javax.swing.*;
 import java.util.ArrayList;
 
 public class ArraysListCarros {
@@ -23,8 +23,23 @@ public class ArraysListCarros {
 
         carros.set(4, "MITSUBICHI");// ele irá localizar a posição 4 e substituir pelo valor entre ""
 
-        
+        carros.remove("VECTRA"); //ele faz a varredura, localizando o procurado, irá remover
+        System.out.println(carros);
 
+        //insere no arraylist um valor string por meio de chamada de tela
+        carros.add(JOptionPane.showInputDialog("Informe a marca do carro"));
+        System.out.println(carros);
 
+        if(carros.contains("COROLLA")){
+            for (int i =0; i < carros.size(); i++){
+                if ("COROLLA".equals(carros.get(i))){
+                    carros.set(i, "BUGATTI");
+                    break;
+                }
+            }
+        }else {
+            System.out.println("Não encontrei o argumentado pesquisado");
+        }
+        System.out.println(carros);
     }
 }
